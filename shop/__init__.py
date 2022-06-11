c1366f72d69ab97803f6c5572231c4b7a35e9055
tom <tomkingori66@gmail.com>
2022-06-11 17:03:45 +0300

    registration complete

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,11 +1,15 @@
+import bcrypt
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 DB_NAME = "myshop.db"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+app.config['SECRET_KEY'] = 'bazubazubazubazu111'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from shop.admin import routes
--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,18 +1,25 @@
 from flask import render_template, session, redirect, request, url_for, flash
-from shop import app, db
+from matplotlib.pyplot import title
+from shop import app, db, bcrypt
 from .forms import RegistrationForm
+from .models import User
 
 @app.route("/")
 def home():
-    return "<p>Home page of shop</p>"
+    return render_template('index.html', title='Admin Page')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
-        # user = User(form.username.data, form.email.data,
-        #             form.password.data)
-        # db_session.add(user)
-        flash('Registration success!!')
-        return redirect(url_for('login'))
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+
+        user = User(name=form.name.data, 
+                    username=form.username.data, 
+                    email=form.email.data,
+                    password=hash_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Thanks for registering {form.name.data}!!', category='success')
+        return redirect(url_for('home'))
     return render_template('register.html', form=form, title='Registration')
